24cba371fcef89d7fb4f6491d0960f307f54703f
sonvau05 <sonvau05@gmail.com>
2026-03-25 19:53:49 +0700

    Add files via upload

---
+++ 23-03/baitap/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import dao.UserDAO;
+import model.User;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserDAO dao = new UserDAO();
+        User user = dao.login(email, password);
+
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+
+            response.sendRedirect("dashboard");
+        } else {
+            response.sendRedirect("login.jsp");
+        }
+    }
+}
---
+++ 23-03/baitap/src/main/webapp/dashboard.jsp
@@ -0,0 +1,5 @@
+<h2>Dashboard</h2>
+Welcome ${sessionScope.user.email}
+
+<br/>
+<a href="settings">Settings</a>
